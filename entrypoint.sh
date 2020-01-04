865dfa93c930ab374f76b29073a32c82faefd224
C.Gong <gong.cheng@newsbreak.com>
2020-01-04 19:12:04 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ cd "$GITHUB_WORKSPACE"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-misspell -locale="${INPUT_LOCALE}" -source=text . \
+misspell -locale="${INPUT_LOCALE}" -source="text" . \
   | reviewdog -efm="%f:%l:%c: %m" -name="misspell" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
